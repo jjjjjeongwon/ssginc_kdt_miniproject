71b62033050c238a451d0fe0c1c810331c4a6020
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-02-09 13:32:09 +0900

    feat: 헤더 뷰 진행중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Header } from './components/layout/Header';
 import Main from './components/pages/Main';
 import './App.css';
+import Header from './components/layout/Header';
 
 function App() {
   return (
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Header = () => {
-  return <Wrap></Wrap>;
+const Header = () => {
+  return (
+    <Wrap>
+      <HeaderLogoWrap>
+        <LogoImage href="/">
+          <img src={'/images/logo.png'} alt="" />
+        </LogoImage>
+      </HeaderLogoWrap>
+      <CartImage>
+        <img src={''} alt="" />
+      </CartImage>
+    </Wrap>
+  );
 };
 
 const Wrap = styled.div`
-  width: 100vw;
-  height: 118px;
-  background-color: aliceblue;
+  /* max-width: 704px;
+  margin: 0 auto 0;
+  width: 100%; */
+  padding-left: 14px;
+  padding-right: 14px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #f4f4f4;
 `;
+
+const HeaderLogoWrap = styled.div`
+  height: 90px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 0 14px 19px 14px;
+  box-sizing: border-box;
+`;
+
+const LogoImage = styled.div`
+  max-width: 98px;
+  max-height: 18px;
+  width: 98px;
+  height: 18px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const CartImage = styled.div`
+  width: 24px;
+  height: 24px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default Header;
